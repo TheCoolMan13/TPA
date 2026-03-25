0106528f29def5720ef9df7e40001eb32cedb685
TheCoolMan13 <titust.ticusan@gmail.com>
2026-03-25 14:39:36 +0200

    forma poloneza complete

--- 20260319/forma_poloneza.c
+++ 20260319/forma_poloneza.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 
+int prioritate(char op)
+{
+    if (op == '*' || op == '/')
+        return 2;
+    if (op == '+' || op == '-')
+        return 1;
+    return 0;
+}
+
 int main()
 {
     char E[256];
-    fgets(E, 256 , stdin);
+    fgets(E, 256, stdin);
     int EC = 0; // <- E counter
 
     char fp[256] = {""}; // <- temp storage
-    int fpC = 0; // <- fp Counter
-    char st[256]; // <- final storage
-    int stC = 0; // <= stack Counter
+    int fpC = 0;         // <- fp Counter
+    char st[256];        // <- final storage
+    int stC = 0;         // <= stack Counter
 
     /*
 
@@ -21,19 +30,48 @@ int main()
 
     (a+b)-(b-c) -> ab+bc--
 
+
+    Intrebare -> se asuma ca toti operanzii sunt in ordine alfabetica??
+    -> asumam ca da, daca nu se poate schimba codul
+
+
+    INPUT
+    (a*(b+c))
+    2
+    3
+    1
+
+    OUTPUT
+    8
     */
+
+    int v[27] = {0};
+    int vC = 0;
+    while (scanf("%d", &v[vC]) == 1)
+    {
+        vC++;
+    }
+    // printf("VC EST : %d \n", vC);
+    //  for (int i = 0; i < vC; i++)
+    //  {
+    //      printf("%d ", v[i]);
+    //  }
 
     while (E[EC] != '\0' && E[EC] != '\n')
     {
-        //printf("%c " , E[EC]);
-        if(E[EC] > 96 && E[EC] < 123)
+        // printf("%c " , E[EC]);
+        if (E[EC] > 96 && E[EC] < 123)
         {
             fp[fpC++] = E[EC++];
             continue;
         }
-        if (strchr(",*/+-" , E[EC]) != NULL)
+        if (strchr(",*/+-", E[EC]) != NULL)
         {
-            //printf("\n operator : %c" , E[EC]);
+            // printf("\n operator : %c" , E[EC]);
+            while (stC > 0 && prioritate(st[stC - 1]) >= prioritate(E[EC]))
+            {
+                fp[fpC++] = st[--stC];
+            }
             st[stC++] = E[EC++];
             continue;
         }
@@ -47,7 +85,7 @@ int main()
             EC++;
             continue;
         }
-        if(E[EC] == '(')
+        if (E[EC] == '(')
         {
             st[stC++] = E[EC++];
             continue;
@@ -55,12 +93,65 @@ int main()
         EC++;
     }
 
+    // Dump la stiva
     while (stC > 0)
     {
         fp[fpC++] = st[--stC];
     }
 
     fp[fpC] = '\0';
-    printf("%s" , fp);
+    // printf("%s", fp);
+
+    int valSt[256]; // <- St pt val
+    int valStC = 0; // <- Ct pt st de val
+
+    for (int i = 0; fp[i] != '\0'; i++)
+    {
+        char curent = fp[i];
+
+        if (curent >= 'a' && curent <= 'z')
+        {
+
+            valSt[valStC++] = v[curent - 'a'];
+        }
+        else if (strchr("+ - * /", curent) != NULL && curent != ' ')
+        {
+            int val_dreapta = valSt[--valStC];
+            int val_stanga = valSt[--valStC];
+            int rezultat = 0;
+
+            switch (curent)
+            {
+            case '+':
+                rezultat = val_stanga + val_dreapta;
+                break;
+            case '-':
+                rezultat = val_stanga - val_dreapta;
+                break;
+            case '*':
+                rezultat = val_stanga * val_dreapta;
+                break;
+            case '/':
+                if (val_dreapta != 0)
+                {
+                    rezultat = val_stanga / val_dreapta;
+                }
+                else
+                {
+                    printf("impartire la zero");
+                    return 1;
+                }
+                break;
+            }
+
+            // push rezultat pe stiva
+            valSt[valStC++] = rezultat;
+        }
+    }
+
+    printf("\n%d\n", valSt[0]);
+
+    // fp[fpC] = '\0';
+    // printf("%s", fp);
     return 0;
 }
